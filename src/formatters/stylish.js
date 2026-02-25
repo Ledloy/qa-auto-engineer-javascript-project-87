7cb666e8d630f8f5731327e630800c4dd279657c
Ledloy <d-os@inbox.ru>
2026-02-26 02:33:35 +0300

    fix: remove quotes from strings in stylish formatter

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -3,44 +3,45 @@ const STATUS = {
   REMOVED: 'removed',
   CHANGED: 'changed',
   UNCHANGED: 'unchanged',
-}
+};
 
 const formatValue = (value) => {
   if (value === null) {
-    return 'null'
+    return 'null';
   }
   if (value === undefined) {
-    return 'undefined'
+    return 'undefined';
   }
   if (typeof value === 'object') {
-    return '[complex value]'
+    return '[complex value]';
   }
   if (typeof value === 'boolean') {
-    return value
+    return value;
   }
   if (typeof value === 'number') {
-    return value
+    return value;
   }
-  return `'${String(value)}'`
-}
+
+  return String(value);
+};
 
 export default (diff) => {
   const lines = diff.map((node) => {
-    const { key, status } = node
+    const { key, status } = node;
 
     if (status === STATUS.ADDED) {
-      return `  + ${key}: ${formatValue(node.value)}`
+      return `  + ${key}: ${formatValue(node.value)}`;
     }
     if (status === STATUS.REMOVED) {
-      return `  - ${key}: ${formatValue(node.value)}`
+      return `  - ${key}: ${formatValue(node.value)}`;
     }
     if (status === STATUS.CHANGED) {
-      const line1 = `  - ${key}: ${formatValue(node.value1)}`
-      const line2 = `  + ${key}: ${formatValue(node.value2)}`
-      return `${line1}\n${line2}`
+      const line1 = `  - ${key}: ${formatValue(node.value1)}`;
+      const line2 = `  + ${key}: ${formatValue(node.value2)}`;
+      return `${line1}\n${line2}`;
     }
-    return `    ${key}: ${formatValue(node.value)}`
-  })
+    return `    ${key}: ${formatValue(node.value)}`;
+  });
 
-  return `{\n${lines.join('\n')}\n}`
-}
+  return `{\n${lines.join('\n')}\n}`;
+};
